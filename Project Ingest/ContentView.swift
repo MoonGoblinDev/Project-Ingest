93ab6192f4bffec417be056b32ad8d49c41374e3
MoonGoblin <bregas.sat1@gmail.com>
2025-07-04 23:38:43 +0800

    Initial Commit

---
+++ Project Ingest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project Ingest
+//
+//  Created by Bregas Satria Wicaksono on 04/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project Ingest/Project_IngestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_IngestApp.swift
+//  Project Ingest
+//
+//  Created by Bregas Satria Wicaksono on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_IngestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
